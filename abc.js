130d89ef6f6c88044065f8e1b1e1f1643dfb90e0
Rhythm-Abhilash <83583199+Rhythm-Abhilash@users.noreply.github.com>
2021-05-03 16:11:07 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,8 @@
+var num1=Number(document.getElementById("number").value);
+function F(){
+num1=Number(document.getElementById("number").value);
+var area1= num1*num1;
+var peri1= num1*4;
+document.getElementById("area").innerHTML = area1;
+document.getElementById("peri").innerHTML = peri1;
+}
